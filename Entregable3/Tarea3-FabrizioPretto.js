68db8cb51fd14f2ae3dc1e81a21aee754d8cb6ba
FabrizioPretto <fgpretto@gmail.com>
2023-10-24 17:23:01 -0300

    v4

--- Entregable3/Tarea3-FabrizioPretto.js
+++ Entregable3/Tarea3-FabrizioPretto.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { products } from './products';
+import { products } from './products.js';
 
 const app = express();
 
@@ -10,6 +10,24 @@ app.get('/', (req, res) => {
 app.get('/home', (req, res) => {
     res.send("Bienvenido");
 })
+
+
+//products?value=1 se accede como req.query.value y llega como string
+
+app.get('/products', (req, res) => {
+    res.send(products);
+    //res.redirect('/home');
+    //res.render('products');
+})
+
+app.get('/filter-products', (req, res) => {
+    console.log(req.query)
+    const { value } = req.query;
+    const productsFilter = products.filter(p => p.price >= parseInt(value));
+    res.json(productsFilter);
+})
+
+//PRECIO MAYOR O IGUAL A: _value_ [BUSCAR] --> GET localhost:8080/products?value=${value}
 
 
 const PORT = 8080;
