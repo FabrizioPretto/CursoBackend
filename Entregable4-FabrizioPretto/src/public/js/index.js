22994c1b0bd564b5e261b58624c57e0d6d90e151
FabrizioPretto <fgpretto@gmail.com>
2023-11-07 17:18:32 -0300

    v5

---
+++ Entregable4-FabrizioPretto/src/public/js/index.js
@@ -0,0 +1,36 @@
+const socketClient = io();
+
+socketClient.on('saludoDesdeBack', (msg) => {
+    console.log(msg);
+
+    socketClient.emit('respuestaDesdeFront', "Muchas gracias")
+});
+
+
+const form = document.getElementById('form');
+const inputName = document.getElementById("name");
+const inputPrice = document.getElementById("price");
+const products = document.getElementById("products");
+
+form.onsubmit = (e) => {
+    e.preventDefault();
+    const name = inputName.value;
+    const price = inputPrice.value;
+    const product = { name, price };
+    socketClient.emit('newProduct', product);
+    inputName.value = "";
+    inputPrice.value = "";
+};
+
+socketClient.on('arrayProducts', (productsArray) => {
+    let infoProducts = '';
+    productsArray.forEach(p => {
+        infoProducts += `${p.name} - $${p.price} </br>`//luego cada producto se agrega al p products del websockets.handlebars 
+
+    });
+    products.innerHTML = infoProducts;
+});
+
+socketClient.on("message", (msg) => {
+    console.log(msg);
+})
--- Entregable4-FabrizioPretto/src/server.js
+++ Entregable4-FabrizioPretto/src/server.js
@@ -15,8 +15,37 @@ app.get('/', (req, res) => {
     res.render('websocket');
 })
 
+app.post('/', (req, res) => {
+    const { msg } = req.body;
+    socketServer.emit('message', msg);
+    res.send("Se envió el mensaje al socket del cliente")
+})
+
+//Este método y la línea de abajo es la generación del servidor websocket
+//la lista de products es similar al ejemplo de la clase de motores de plantillas. Se hace con un res.render
 const httpServer = app.listen(8080, () => {
     console.log("Escuchando al puerto 8080");
 });
 
 const socketServer = new Server(httpServer);
+
+const products = [];//luego voy pusheando
+
+
+
+socketServer.on('connection', (socket) => {
+    console.log(`Usuario conectado ${socket.id}`);
+
+    socket.on('disconnect', () => console.log(`Usuario desconectado ${socket.id}`));
+
+    socket.emit('saludoDesdeBack', "Bienvenido a websocket");
+
+    socket.on('respuestaDesdeFront', (msg) => {
+        console.log(msg);
+    });
+
+    socket.on('newProduct', (product) => {
+        products.push(product);
+        socketServer.emit('arrayProducts', products);
+    })
+})
