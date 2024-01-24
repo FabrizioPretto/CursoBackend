1704e6089235590c995e777695716474b0ef32cd
FabrizioPretto <fgpretto@gmail.com>
2024-01-23 23:42:29 -0300

    Update ticketServices.js

--- proyectoFinal/src/services/ticketServices.js
+++ proyectoFinal/src/services/ticketServices.js
@@ -1,16 +1,21 @@
 import TicketMongoDao from "../persistence/daos/mongodb/tickets/ticketDao.js";
 const ticketDao = new TicketMongoDao();
+import ProductService from "./productServices.js";
+const prodServices = new ProductService();
+import UserServices from "./userServices.js";
+const userServices = new UserServices();
 
 import { getById } from "./cartServices.js";
-import { getUserById } from "./userServices.js";
-import { getProductById as getProdById } from "./productServices.js";
+//import { getUserById } from "./userServices.js";
+//import { getProductById as getProdById } from "./productServices.js";
+
 
 import { v4 as uuidv4 } from 'uuid';
 
 export const generateTicket = async (userId, cartId) => {
     try {
         //buscar el usuario
-        const user = await getUserById(userId);
+        const user = await userServices.getUserById(userId);
         if (!user) return false;
         //buscar el carrito
         const cart = await getById(cartId);
@@ -20,7 +25,7 @@ export const generateTicket = async (userId, cartId) => {
 
         for (const p of cart.products) {
             const idProd = p.product._id.toString();
-            const prodFromDB = await getProdById(idProd);
+            const prodFromDB = await prodServices.getProductById(idProd);
             //verifico si la cantidad del carrito supera al stock del producto en db
             if (p.quantity <= prodFromDB.stock) {
                 const amount = p.quantity * prodFromDB.price;
